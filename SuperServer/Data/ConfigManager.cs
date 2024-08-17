c8420a2bbbfbc93ed56b3500c64854992db95ba9
정채운 <toyboy01113@gmail.com>
2024-08-17 18:51:58 +0900

    초기 커밋.

---
+++ SuperServer/Data/ConfigManager.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperServer.Data
+{
+    public struct ConfigData
+    {
+        public string ip;
+        public int port;
+    }
+    public class ConfigManager
+    {
+        public static ConfigData Config { get; private set; }
+        
+
+        public void LoadConfigData(string path = "./config.json")
+        {
+            string text = File.ReadAllText(path);
+            Config = JsonConvert.DeserializeObject<ConfigData>(text);
+        }
+    }
+}
---
+++ SuperServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace SuperServer
+{
+    internal class Program
+    {
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
